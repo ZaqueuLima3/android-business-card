5aa0eb12d1f0030f6fd437a096d34552b8a0ad6b
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-04 21:11:30 -0300

    feat: list and share business card

---
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,45 @@
+package com.example.businesscard.ui
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.businesscard.App
+import com.example.businesscard.R
+import com.example.businesscard.data.BusinessCard
+import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener() {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+
+        binding.btnConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                name = binding.tilName.editText?.text.toString(),
+                company = binding.tilCompany.editText?.text.toString(),
+                phone = binding.tilPhone.editText?.text.toString(),
+                job = binding.tilJob.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                background = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+}
